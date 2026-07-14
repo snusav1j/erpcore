dcac1a881f26b9392887a55958b0d9b9ce5f1f32
snusav1j <snusav1j@gmail.com>
2026-07-14 16:57:52 +0300

    fix migrations

--- db/migrate/20260714110450_change_client_and_interaction_types.rb
+++ db/migrate/20260714110450_change_client_and_interaction_types.rb
@@ -1,5 +1,6 @@
 class ChangeClientAndInteractionTypes < ActiveRecord::Migration[8.0]
   def change
+    
     rename_column :clients, :status, :status_id
 
     remove_column :clients, :client_type, :integer
